4a0d0c85014030e7869dbb344de7fbbebd8998db
JoaoPedroLage <JoaoPedro.Lage@outlook.com>
2021-11-24 18:11:51 -0300

    Requisito 1: Corrigi o CSS

--- src/redux/actions/loginAction.js
+++ src/redux/actions/loginAction.js
@@ -1,4 +1,7 @@
 export const SET_USER = 'SET_USER';
+export const REQUEST_API = 'REQUEST_API';
+export const RECEIVED_API = 'RECEIVED_API';
+export const FAILED_REQUEST = 'FAILED_REQUEST';
 
 export const setUser = (name, email) => ({
   type: SET_USER,
--- src/redux/actions/longinAction.js
+++
@@ -1,7 +0,0 @@
-export const SET_USER = 'SET_USER';
-
-export const setUser = (name, email) => ({
-  type: SET_USER,
-  name,
-  email,
-});
